0048c94e671ea232648ecaf521b2990b3824d351
Heshy Roskes <jroskes1@jhu.edu>
2017-01-26 15:52:40 -0500

    move

--- script/converttotemplate.sh
+++
@@ -1,6 +0,0 @@
-infile=$1
-outfile=${infile/_cfg.py/_cfg_template.py}
-cat $infile | sed "s/\(fileNames = .*root')\)/\1,/" |
-              sed "/fileNames = .*root'),/a eventsToProcess = cms.untracked.VEventRange('1:FIRSTEVENT-1:LASTEVENT')" |
-              sed "s/^events/    events/" |
-              sed "s/\(fileName = .*\)\.root'),/\1_JOBNUMBER.root'),/" > $outfile
